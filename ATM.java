069925dd80a4e21edc49c5d03fa76dc017b51b26
Mohammadreza Sheikholeslami <sheikholeslamimohammadreza@outlook.com>
2024-05-14 22:01:59 +0330

    Update ATM new

--- ATM.java
+++ ATM.java
@@ -361,6 +361,7 @@ public class ATM {
         cash.add(cashsubmit);
         cash.add(BackGroundimageCash);
 
+
 //////////////////////////////  Transfer Page Components /////////////////////////////////////////
 
         ImageIcon imagetransferpage = new ImageIcon("Test3.jpg");
